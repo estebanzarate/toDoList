47521b253b465fb85c55b450c023f0a699f188cf
estebanzarate <esteban-zarate@hotmail.com>
2022-03-14 12:16:27 -0300

    focus

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-const inputItem = document.querySelector("#inputItem"),
-    btnAdd = document.querySelector("#btnAdd"),
-    ul = document.querySelector("#ul");
+document.addEventListener("DOMContentLoaded", () => {
 
-window.addEventListener("click", agregarItem);
+    const inputItem = document.querySelector("#inputItem"),
+        btnAdd = document.querySelector("#btnAdd"),
+        ul = document.querySelector("#ul");
 
-//Agrega una tarea nueva
-function agregarItem(e) {
-    if (inputItem.value !== "") {
-        if (e.target.matches("#btnAdd")) {
+    inputItem.focus();
+    window.addEventListener("click", e => {
+        if (e.target.matches("body")) inputItem.focus();
+    })
+
+    btnAdd.addEventListener("click", agregarItem);
+
+    inputItem.addEventListener("keypress", e => {
+        if (e.code == "Enter" || e.code == "NumpadEnter") return agregarItem();
+    })
+
+    //Agrega una tarea nueva
+    function agregarItem() {
+        if (inputItem.value !== "") {
             const li = document.createElement("li");
             li.textContent = inputItem.value;
             ul.appendChild(li);
             inputItem.value = "";
+            inputItem.focus();
         }
     }
-}
+
+})
