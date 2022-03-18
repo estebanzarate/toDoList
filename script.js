36394331af05feeaa2fc3c8ade2ea3435a326ecc
estebanzarate <esteban-zarate@hotmail.com>
2022-03-18 12:36:17 -0300

    botón para eliminar tarea

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const inputItem = document.querySelector("#inputItem"),
         btnAdd = document.querySelector("#btnAdd"),
-        ul = document.querySelector("#ul");
+        ul = document.querySelector("#ul"),
+        btnTrash = `<i class="fa-solid fa-trash-can"></i>`;
 
     inputItem.focus();
     window.addEventListener("click", e => {
@@ -10,19 +11,29 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     btnAdd.addEventListener("click", agregarItem);
+    window.addEventListener("click", eliminarItem);
 
     inputItem.addEventListener("keypress", e => {
         if (e.code == "Enter" || e.code == "NumpadEnter") return agregarItem();
     })
 
-    //Agrega una tarea nueva
+    //Agrega tarea nueva
     function agregarItem() {
         if (inputItem.value !== "") {
             const li = document.createElement("li");
+            li.classList.add("li");
             li.textContent = inputItem.value;
+            li.innerHTML += btnTrash;
             ul.appendChild(li);
             inputItem.value = "";
             inputItem.focus();
+        }
+    }
+
+    //Elimina tarea
+    function eliminarItem(e) {
+        if (e.target.matches(".fa-trash-can")) {
+            e.target.parentElement.remove();
         }
     }
 
